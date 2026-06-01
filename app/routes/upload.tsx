4757018e18478ab58a701d4c51634693b851691e
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2026-06-01 12:37:57 +0530

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -65,6 +65,7 @@ const upload = () => {
         await kv.set(`resume:${uuid}`,JSON.stringify(data));
         setStatusText('Analysis complete! Redirecting...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
